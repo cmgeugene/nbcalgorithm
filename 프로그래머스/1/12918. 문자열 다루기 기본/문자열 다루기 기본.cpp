97088729c73484a0cbaa84260ce3ed4208398bd1
cmgeugene <jscj0815@gmail.com>
2025-01-13 09:45:07 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 bool solution(string s) {
     bool answer = true;
-    if(s.size() != 4 && s.size() != 6)
-        return false;
     for(char t : s){
         if((int)t <= 47 || (int)t >= 59)
             return false;
     }
+    if(s.size() != 4 && s.size() != 6)
+        return false;
     return answer;
 }
