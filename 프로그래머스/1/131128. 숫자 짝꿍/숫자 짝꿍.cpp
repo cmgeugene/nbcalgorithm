de5adea2c5a80dde66ac2204f87dc3f43b424fc4
cmgeugene <jscj0815@gmail.com>
2025-02-10 10:25:19 +0900

    [level 1] Title: 숫자 짝꿍, Time: 214.50 ms, Memory: 29 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string X, string Y) {
+    string answer = "";
+    int x[10] = {0};
+    int y[10] = {0};
+    
+    for(int i = 0; i < X.size(); i++)
+    {
+        x[X[i] - '0']++;
+    }
+    for(int i = 0; i < Y.size(); i++)
+    {
+        y[Y[i] - '0']++;
+    }
+    
+    for(int i = 9; i >= 0; i--)
+    {
+        int minval = min(x[i], y[i]);
+        for(int j = 0; j<minval; j++)
+        {
+            answer += to_string(i);    
+        }
+        
+    }
+    if(answer == "") return "-1";
+    else if(answer[0] == '0') return "0";
+    else return answer;
+}
