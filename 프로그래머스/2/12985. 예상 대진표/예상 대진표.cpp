daa34653675d3ed07febcfb4d19cef9d7ddab6f6
cmgeugene <jscj0815@gmail.com>
2025-02-27 09:44:26 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+    int round = 1;
+    while(n)
+    {   
+        if(n == 2) return round;
+        if(a % 2 == 0 && b == a - 1)
+        {
+            return round;
+        }
+        else if(b % 2 == 0 && a == b - 1)
+        {
+            return round;
+        }
+        
+        a % 2 == 0 ? a/=2 : a = (a + 1) / 2;
+        b % 2 == 0 ? b/=2 : b = (b + 1) / 2;
+        round++;
+        
+        if(a % 2 == 0 && b == a - 1)
+        {
+            return round;
+        }
+        else if(b % 2 == 0 && a == b - 1)
+        {
+            return round;
+        }
+    }
+}
