180a2a4586996d01e3005eb2c1985e6ddad4363e
cmgeugene <jscj0815@gmail.com>
2025-10-11 12:17:47 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.cc
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -8,13 +7,10 @@ int main()
     int A, B;
     cin >> A >> B;
     
-    string _B;
-    _B = to_string(B);
-    
     int b1, b10, b100;
-    b1 = _B[2] -'0';
-    b10 = _B[1] -'0';
-    b100 = _B[0] -'0';
+    b1 = B % 10;
+    b10 = (B/10) % 10;
+    b100 = B / 100;
     
     cout << A*b1 << endl <<
         A*b10 << endl <<
