3f54c0fc5537a9c679c8b921894cdcc670ddb2af
cmgeugene <jscj0815@gmail.com>
2025-11-13 21:34:35 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,10 +1,6 @@
 def solution(num_list):
-    answer = list()
-    index = 0
-    for i in num_list :
-        if i < 0 : 
-            answer.append(index)
-            return answer[0]
-        index += 1
+    for index, value in enumerate(num_list) :
+        if value < 0 : 
+            return index
     return -1
     
